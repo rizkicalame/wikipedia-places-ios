56af8a22eb24daf54a18b176140386cc2fc8345f
Rizki Calame <rtj.calame@gmail.com>
2024-08-21 19:12:40 +0200

    feat: proj init

---
+++ wikipedia-places-ios/wikipedia-places-ios/wikipedia_places_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  wikipedia_places_iosApp.swift
+//  wikipedia-places-ios
+//
+//  Created by Rizki Calame on 21/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct wikipedia_places_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
